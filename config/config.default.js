358fefff93e25c54464f9643ea0df34724d03d8c
zhongchubing <861013016@qq.com>
2023-02-08 16:22:24 +0800

    使用fs-baofeng assignment结构

--- config/config.default.js
+++ config/config.default.js
@@ -21,7 +21,7 @@ module.exports = appInfo => {
     appLogo: `${appId}/public/img/logo.png`,
     appType: 'single',
     appDirectoryLink: '/',
-    indexPage: `/${appId}/page/articleManagement`,
+    indexPage: `/${appId}/page/assignmentManagement`,
     loginPage: `/${appId}/page/login`,
     helpPage: `/${appId}/page/help`,
     uploadDir,
