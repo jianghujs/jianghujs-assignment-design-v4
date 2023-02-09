c9cd930236339aeb72e9c7cd100a147a9f642a41
zhongchubing <861013016@qq.com>
2023-02-09 14:23:28 +0800

    变量优化

--- app/service/assignment.js
+++ app/service/assignment.js
@@ -1,49 +1,9 @@
 'use strict';
 const Service = require('egg').Service;
-const { tableEnum, articlePublishStatusEnum } = require("../constant/constant");
-const _ = require("lodash");
-const path = require("path");
-
-// TODO: 封装一下
-const dayjs = require("dayjs");
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
-dayjs.extend(utc)
-dayjs.extend(timezone)
-
 const idGenerateUtil = require("@jianghujs/jianghu/app/common/idGenerateUtil");
-const validateUtil = require("@jianghujs/jianghu/app/common/validateUtil");
-const { BizError, errorInfoEnum } = require("../constant/error");
-const fs = require("fs"),
-    fsPromises = require("fs").promises,
-    unlink = fsPromises.unlink,
-    rename = fsPromises.rename,
-    util = require("util"),
-    exists = util.promisify(fs.exists);
-const actionDataScheme = Object.freeze({
-  deletedArticle: {
-    type: "object",
-    additionalProperties: true,
-    required: ["articleId"],
-    properties: {
-      articleId: { anyOf: [{ type: "string" }, { type: "number" }] },
-    },
-  },
-  restoreArticle: {
-    type: "object",
-    additionalProperties: true,
-    required: ["articleId"],
-    properties: {
-      articleId: { anyOf: [{ type: "string" }, { type: "number" }] },
-    },
-  },
-});
-
 class AssignmentService extends Service {
 
   async fillInsertItemParamsBeforeHook() {
-    const { userId, username } = this.ctx.userInfo;
-    const time = dayjs().format();
     const tableName = "assignment";
     const columnName = "assignmentId";
     const assignmentId = await idGenerateUtil.idPlus({
